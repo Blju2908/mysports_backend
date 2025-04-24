448a93cea8bfa3af6ce3056b3ad665cf170d57fe
Blju2908 <38442972+Blju2908@users.noreply.github.com>
2025-04-24 08:39:08 +0200

    lösung problem mit api key

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -19,3 +19,15 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         case_sensitive = True 
+
+# Create a singleton instance
+_settings = None
+
+def get_config() -> Settings:
+    """
+    Returns a singleton instance of the Settings class.
+    """
+    global _settings
+    if _settings is None:
+        _settings = Settings()
+    return _settings 
